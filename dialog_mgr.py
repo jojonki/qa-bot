bd8edaffd84ef9f13bea8372f9808c999cba0700
jojonki <junki.ohmura@gmail.com>
2017-12-07 13:56:28 -0500

    add topic mode

--- dialog_mgr.py
+++ dialog_mgr.py
@@ -1,12 +1,38 @@
 bye_words = ['bye', 'good bye', 'see you']
 
 
-class DialogManager():
+class DBRetriever():
     def __init__(self):
         pass
 
-    def get_action(self, u):
-        if u in bye_words:
+    def search_topic_by_uttr(self, user_uttr):
+        return "iPod"
+
+    def search_topic_by_title(self, topic_title):
+        pass
+
+
+class DialogManager():
+    def __init__(self):
+        self.db_retr = DBRetriever()
+
+    def get_action(self, uttr, in_topic):
+        sys_uttr = "I'm sorry. I cannot understand what you said"
+        topic_end = False
+        dlg_end = False
+        if uttr in bye_words:
             return 'Bye', True
         else:
-            return "I'm sorry. I cannot understand what you said", False
+            if in_topic:
+                pass
+            else:
+                # try to retrieve a topic what user said
+                topic = self.db_retr.search_topic_by_uttr(uttr)
+                if topic is None:
+                    sys_uttr = "I cannot find a topic what you want to ask"
+                else:
+                    sys_uttr = "OK. I know a lot about \"{}\". Ask Me Anything!".format(topic)
+
+        return {'sys_uttr': sys_uttr,
+                'topic_end': topic_end,
+                'dlg_end': dlg_end}
--- runner.py
+++ runner.py
@@ -1,23 +1,21 @@
 from dialog_mgr import DialogManager
 
 dm = DialogManager()
-
+in_topic = False
 
 def s_print(uttr):
     print('S:', uttr)
 
 
 def run_console():
-    u = input("S: Hi! My name is AMA. Yo can ask me anything.\nU: ")
-    u = u.lower()
-    act, end = dm.get_action(u)
-    s_print(act)
+    print("S: Hi! My name is AMA. Yo can ask me anything.")
+    print("   What kind of topic do you want to talk about?")
     while True:
         u = input("U: ")
         u = u.lower()
-        act, end = dm.get_action(u)
-        s_print(act)
-        if end:
+        act = dm.get_action(u, in_topic)
+        s_print(act['sys_uttr'])
+        if act['dlg_end']:
             break
 
 run_console()
